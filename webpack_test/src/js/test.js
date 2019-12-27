531bab92b07f80e4d2ef239799fc3d8fa3d701ad
qinuyu <1326609017@qq.com>
2019-12-27 14:27:24 +0800

    webpack测试

---
+++ webpack_test/src/js/test.js
@@ -0,0 +1,5 @@
+import {foo,bar} from './jisuan'
+import '../css/test.css'
+console.log("success !");
+console.log(foo(3));
+console.log(bar(3));
---
+++ webpack_test/webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+  mode:'development',
+  entry: './src/js/test.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist/js')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [ 'style-loader', 'css-loader' ]
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192
+            }
+          }
+        ]
+      }
+    ]
+  }
+};
